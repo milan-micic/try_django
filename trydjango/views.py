fe34e810a759cdc17aa9a21cfa7d717d49a01dd7
milan-micic <milan2micic@yahoo.com>
2025-03-02 21:42:11 +0100

    finished model articles

--- trydjango/views.py
+++ trydjango/views.py
@@ -1,24 +1,18 @@
-"""
-To render html web pages
-"""
-
 from django.http import HttpResponse
+from django.template.loader import render_to_string
+from articles.models import Article
 
-name = "Sandman"
+article_obj = Article.objects.get(id=2)
+article_queryset = Article.objects.all()
 
-H1_STRING = f"""
-<h1>Hello {name}</h1>
-"""
-P_STRING = f"""
-<p>Hello {name}</p>
-"""
+context = {
+  "object_list": article_queryset,
+  "id": article_obj.id,
+  "title": article_obj.title,
+  "content": article_obj.content
+}
 
-HTML_STRING = H1_STRING + P_STRING
+HTML_STRING = render_to_string("home-view.html",context=context)
 
 def home_view(request):
-  """
-  Take in a request (Django sends request)
-  Return HTML as a response (We pick to return the response)
-  """
-
   return HttpResponse(HTML_STRING)
