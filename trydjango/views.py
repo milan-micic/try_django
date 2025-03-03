8dffe50ef83a616c342c03082dcb638780f68881
milan-micic <milan2micic@yahoo.com>
2025-03-03 20:51:13 +0100

    zavrsena forma za kreiranje articla

--- trydjango/views.py
+++ trydjango/views.py
@@ -2,17 +2,16 @@ from django.http import HttpResponse
 from django.template.loader import render_to_string
 from articles.models import Article
 
-article_obj = Article.objects.get(id=2)
-article_queryset = Article.objects.all()
-
-context = {
-  "object_list": article_queryset,
-  "id": article_obj.id,
-  "title": article_obj.title,
-  "content": article_obj.content
-}
-
-HTML_STRING = render_to_string('home-view.html',context=context)
-
 def home_view(request):
+  article_obj = Article.objects.get(id=2)
+  article_queryset = Article.objects.all()
+
+  context = {
+    "object_list": article_queryset,
+    "id": article_obj.id,
+    "title": article_obj.title,
+    "content": article_obj.content
+  }
+
+  HTML_STRING = render_to_string('home-view.html',context=context)
   return HttpResponse(HTML_STRING)
